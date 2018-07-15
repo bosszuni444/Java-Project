a00bf21ca13b1e848489322a7e3ff08314de9f95
bosszuni444 <41098696+bosszuni444@users.noreply.github.com>
2018-07-15 21:41:33 +0800

    Add files via upload

---
+++ DoubleColorBall.java
@@ -0,0 +1,15 @@
+class menu{};
+class color{};
+class number{};
+
+public class DoubleColorBall {
+
+	
+	public static void selectNumber() {
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
